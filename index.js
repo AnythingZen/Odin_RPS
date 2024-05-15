afaa35fb921edb6d01d7a3baf1c5caadd73c6adc
anythingzen <chengzhanxd@gmail.com>
2024-05-15 21:18:50 +0800

    Added a UI for the RPS game. It allows user to choose which RPS to use, and displays its score, finally showing a winner once a player hits 5 points.

--- index.js
+++ index.js
@@ -21,16 +21,24 @@ function getComputerChoice () {
 }
 
 function getHumanChoice () {
-    let choice = prompt("rock, paper or scissors?").toLowerCase();
-    const rps = ['rock', 'paper', 'scissors'];
+    let btnContainer = document.querySelector('.button-container');
 
-    while (!rps.includes(choice)) {
-        console.log('Choose only rock, paper or scissors');
-        choice = prompt("rock, paper or scissors?").toLowerCase();
-    }
-    console.log('human: ' + choice);
-    return choice;
+    btnContainer.addEventListener('click', (event) => {
+
+        let target = event.target;
+        
+        if (target.tagName === 'BUTTON') {
+            const humanSelection = target.classList.value;
+            const computerSelection = getComputerChoice(); 
+
+            playRound(humanSelection, computerSelection);
+            winnerCondition();
+        }
+    });
 }
+
+let div = document.createElement('div');
+document.querySelector('body').appendChild(div);
 
 function playRound (humanChoice, computerChoice) {
     var dict_rps = {'rock' : 2, 'scissors' : 1, 'paper' : 0};
@@ -39,31 +47,45 @@ function playRound (humanChoice, computerChoice) {
     var com = dict_rps[computerChoice];
 
     if ((human - com) == 1 || (human - com) == -2) {
+        
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
         humanScore += 1;
+        div.textContent = `You win! ${humanChoice} beats ${computerChoice},
+                        score of human: ${humanScore} : com: ${computerScore}`;
     }
     else if (com == human) {
+        
         console.log(`You tied! ${humanChoice} ties with ${computerChoice}`);
+        div.textContent = `You tied! ${humanChoice} ties with ${computerChoice},
+                        score of human: ${humanScore} : com: ${computerScore}`;
     }
     else {
+        
         console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
         computerScore += 1;
+        div.textContent = `You lost! ${computerChoice} beats ${humanChoice},
+                        score of human: ${humanScore} : com: ${computerScore}`;
     }
 }
 
-function playGame () {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice(); 
+getHumanChoice();
 
-    playRound(humanSelection, computerSelection);
-}
+function winnerCondition () {
 
-for (let i = 0; i < 5; i++) {
-    playGame();
-}
-if (humanScore > computerScore) {
-    console.log(`You won with a score of ${humanScore} to ${computerScore}`);
-}
-else {
-    console.log(`You lost with a score of ${humanScore} to ${computerScore}`);
+    if (humanScore > 4) {
+        div.textContent = `You won with a score of ${humanScore} to ${computerScore}`;
+        console.log(`You won with a score of ${humanScore} to ${computerScore}`);
+
+        humanScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore > 4){
+        div.textContent = `You lost with a score of ${humanScore} to ${computerScore}`;
+        console.log(`You lost with a score of ${humanScore} to ${computerScore}`);
+
+        humanScore = 0;
+        computerScore = 0;
+    }
+
+    document.querySelector('body').appendChild(div);
 }
